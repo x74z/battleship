4961d0c573aa81f02c9c599e88584480334e4aed
x74z <166341818+x74z@users.noreply.github.com>
2025-08-07 17:06:24 +0100

    Allow ships to store their placed coordinates

--- src/ship.js
+++ src/ship.js
@@ -4,6 +4,7 @@ export default class Ship {
     this.numOfHits = 0;
     this.isHit = false;
     this.isSunked = false;
+    this.placedCoords = [];
   }
   isSunk() {
     if (this.numOfHits === this.length) {
@@ -14,5 +15,46 @@ export default class Ship {
   hit() {
     this.numOfHits += 1;
     this.isSunk();
+  }
+  validateGivenCoords(coords) {
+    if (coords.length !== this.length) {
+      throw new Error("Number of coordinates === length.");
+    }
+
+    if (
+      // Checks that every row and column has the same number.
+      !coords.every((e) => e[0] === coords[0][0]) &&
+      !coords.every((e) => e[1] === coords[0][1])
+    ) {
+      throw new Error("Ship is not in a straight line");
+    }
+
+    let lastCoord;
+    coords.forEach((currentCoord) => {
+      if (lastCoord === undefined) {
+        console.log("once");
+        lastCoord = currentCoord;
+        return;
+      }
+      if (
+        Math.abs(lastCoord[0] - currentCoord[0]) > 1 ||
+        Math.abs(lastCoord[1] - currentCoord[1]) > 1
+      ) {
+        throw new Error("Coordinates are not valid");
+      }
+      // console.log(`prev: ${lastCoord}, curr: ${currentCoord}`)
+      lastCoord = currentCoord;
+    });
+    return true;
+  }
+
+  placeAtCoords(coords) {
+    // This function assumes that the given coordinates are within the 10x10 grid.
+    this.validateGivenCoords(coords);
+    this.placedCoords = [];
+
+    coords.forEach((e) => {
+      this.placedCoords.push(e);
+    });
   }
 }
--- src/ship.test.js
+++ src/ship.test.js
@@ -5,9 +5,11 @@ describe("ship", () => {
   beforeAll(() => {
     ship = new Ship(3);
   });
+
   test("correct length", () => {
     expect(ship.length).toBe(3);
   });
+
   test("gets hit", () => {
     ship.hit();
     expect(ship.numOfHits).toBe(1);
@@ -16,7 +18,56 @@ describe("ship", () => {
     ship.hit();
     expect(ship.numOfHits).toBe(3);
   });
+
   test("can be sunk", () => {
     expect(ship.isSunked).toBe(true);
   });
+
+  test("can be placed", () => {
+    ship.placeAtCoords([
+      [0, 0],
+      [0, 1],
+      [0, 2],
+    ]);
+    expect(ship.placedCoords).toEqual([
+      [0, 0],
+      [0, 1],
+      [0, 2],
+    ]);
+  });
+
+  test("can't be placed at wrong coords", () => {
+    expect(() => {
+      ship.placeAtCoords([
+        [12391239321, 12837912321],
+        [1221, 9],
+        [24, 21921],
+      ]);
+    }).toThrow(Error);
+    expect(() => {
+      ship.placeAtCoords([
+        [0, 0],
+        [0, 0],
+        [0, 2],
+      ]);
+    }).toThrow(Error);
+  });
+
+  test("can't be placed in anything other than a straight line", () => {
+    expect(() => {
+      ship.placeAtCoords([
+        [0, 0],
+        [0, 1],
+        [1, 1],
+      ]);
+    }).toThrow("Ship is not in a straight line");
+    expect(() => {
+      ship.placeAtCoords([
+        [1, 1],
+        [1, 2],
+        [2, 2],
+      ]);
+    }).toThrow("Ship is not in a straight line");
+  });
+
 });
