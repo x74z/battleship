c8c4dbfaca9824a85a887380e83d8696272c18fe
x74z <166341818+x74z@users.noreply.github.com>
2025-08-09 19:32:03 +0100

    feat: make the dom handler a class

--- src/dom-modules/color-cells.js
+++
@@ -1,9 +0,0 @@
-export default function colorCellsInBoard(coords, target = "player") {
-  // coords is an array of arrays. [[0,0], [0,1], ...]
-  const board = document.querySelector(`.js-battlefield__board--${target}`);
-
-  coords.forEach((coord) => {
-    const cell = board.querySelector( `.js-battlefield__table-cell[data-x="${coord[0]}"][data-y="${coord[1]}"]`);
-    cell.classList.add("battlefield__table-cell--occupied");
-  });
-}
--- src/index.js
+++ src/index.js
@@ -2,13 +2,14 @@ import Gameboard from "./gameboard.js";
 import Ship from "./ship.js";
 import Player from "./player.js";
 import "./styles.css";
-import colorCellsInBoard from "./dom-modules/color-cells.js";
+import DomHandler from "./dom-handler.js";
 
 // todo: add something to make the ships appear on the table. (dom module)
 
 (() => {
   // Test if i can add the classes to the cells for each ship
+  const domHandler = new DomHandler
   const player1 = new Player();
   player1.gb.populateBoardForRegularGame();
-  colorCellsInBoard(player1.gb.getPlacedShipsCoordinates(), "player");
+  domHandler.colorCellsInBoard(player1.gb.getPlacedShipsCoordinates(), "player");
 })();
