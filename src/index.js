70467a7c098d70c50b412219b5068bcb63f159ea
x74z <166341818+x74z@users.noreply.github.com>
2025-08-09 18:37:57 +0100

    feat: add some initial functions to test dom-modules

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
 import Gameboard from "./gameboard.js";
 import Ship from "./ship.js";
-import "./styles.css"
+import Player from "./player.js";
+import "./styles.css";
+import colorCellsInBoard from "./dom-modules/color-cells.js";
 
 // todo: add something to make the ships appear on the table. (dom module)
+
+(() => {
+  // Test if i can add the classes to the cells for each ship
+  const player1 = new Player();
+  player1.gb.populateBoardForRegularGame();
+  colorCellsInBoard(player1.gb.getPlacedShipsCoordinates(), "player");
+})();
