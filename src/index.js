ec0e5abe6c5c777c54d870144175c3ee4ea66a68
x74z <166341818+x74z@users.noreply.github.com>
2025-08-10 15:40:36 +0100

    feat: clean up index.js to make testing easier

--- src/index.js
+++ src/index.js
@@ -4,14 +4,21 @@ import Player from "./player.js";
 import "./styles.css";
 import DomHandler from "./dom-handler.js";
 
-// todo: add something to make the ships appear on the table. (dom module)
-
+function initBoard(domHandler, target = "player", showShipCells = false) {
+  // These functions are just for testing, clean this up later
+  const playerClass = new Player();
+  playerClass.gb.populateBoardForRegularGame();
+  if (showShipCells) {
+    domHandler.colorShipCellsInBoard(
+      playerClass.gb.getPlacedShipsCoordinates(),
+      target,
+    );
+  }
+  domHandler.addClickListenersToCells(playerClass.gb, target);
+}
 (() => {
   // Test if i can add the classes to the cells for each ship
-  const domHandler = new DomHandler
-  const player1 = new Player();
-  player1.gb.populateBoardForRegularGame();
-  domHandler.colorCellsInBoard(player1.gb.getPlacedShipsCoordinates(), "player");
-
-  domHandler.addClickListenersToCells(player1.gb, "player")
+  const domHandler = new DomHandler();
+  initBoard(domHandler, "player", true);
+  initBoard(domHandler, "enemy", false);
 })();
